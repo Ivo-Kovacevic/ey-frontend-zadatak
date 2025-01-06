d0115b9dbf95e84ba0e79415a47eaaad00f284a9
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2025-01-06 17:03:34 +0100

    add gallery skeleton

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,80 +1,77 @@
 import { useEffect, useState } from "react";
-import catImage from "/src/images/meow.png";
-import dogImage from "/src/images/woof.png";
 import H1 from "../H1";
 import H2 from "../H2";
 import ContentSkeleton from "./ContentSkeleton";
 
 export default function Content() {
   const [loading, setLoading] = useState(true);
-  const [loadedImages, setLoadedImages] = useState(0);
 
   useEffect(() => {
-    if (loadedImages === 2) {
-      setLoading(false);
-    }
-  }, [loadedImages]);
+    const loadImage = async () => {
+      const catImage = new Image();
+      const dogImage = new Image();
+      catImage.src = "/src/images/meow.png";
+      dogImage.src = "/src/images/woof.png";
+      catImage.onload = dogImage.onload = () => setLoading(false);
+    };
+
+    loadImage();
+  }, []);
 
   return (
     <>
-      {loading && <ContentSkeleton />}
-      <section
-        className={`bg-gray-100 p-4 flex flex-col gap-4 transition-opacity duration-300 ${loading ? "opacity-0" : "opacity-100"}`}
-      >
-        <H1>Daily pet facts - Love!</H1>
+      {loading ? (
+        <ContentSkeleton />
+      ) : (
+        <section
+          className={`flex flex-col gap-4 bg-gray-100 p-4 transition-opacity duration-300 ${loading ? "opacity-0" : "opacity-100"}`}
+        >
+          <H1>Daily pet facts - Love!</H1>
 
-        <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row">
-          <img
-            src={catImage}
-            alt="closeup picture of a cat"
-            className="rounded-xl"
-            onLoad={() => setLoadedImages((prev) => prev + 1)}
-          />
+          <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row">
+            <img src="/src/images/meow.png" alt="closeup picture of a cat" className="rounded-xl" />
 
-          <div className="flex flex-col justify-between gap-8 md:gap-0">
-            <H2>The cat</H2>
-            <p>
-              Cats have been domesticated for around 4,000 years. While they were once valued for
-              their hunting abilities, they are now valued for their companionship and loving
-              behaviour.
-            </p>
-            <p>
-              While not well known, the collective nouns used for cats and kittens are a clowder of
-              cats and a kindle of kittens.
-            </p>
-            <p>
-              Our domestic cats are known as little cats. They differ from large cats such as lions
-              and tigers because they are naturally active at night and can purr.
-            </p>
-            <p>Cats are now the most popular pet in the UK and in the US.</p>
-          </div>
-        </article>
+            <div className="flex flex-col justify-between gap-8 md:gap-0">
+              <H2>The cat</H2>
+              <p>
+                Cats have been domesticated for around 4,000 years. While they were once valued for
+                their hunting abilities, they are now valued for their companionship and loving
+                behaviour.
+              </p>
+              <p>
+                While not well known, the collective nouns used for cats and kittens are a clowder
+                of cats and a kindle of kittens.
+              </p>
+              <p>
+                Our domestic cats are known as little cats. They differ from large cats such as
+                lions and tigers because they are naturally active at night and can purr.
+              </p>
+              <p>Cats are now the most popular pet in the UK and in the US.</p>
+            </div>
+          </article>
 
-        <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row-reverse">
-          <img
-            src={dogImage}
-            alt="closeup picture of a dog"
-            className="rounded-xl"
-            onLoad={() => setLoadedImages((prev) => prev + 1)}
-          />
+          <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row-reverse">
+            <img src="/src/images/woof.png" alt="closeup picture of a dog" className="rounded-xl" />
 
-          <div className="flex flex-col justify-between gap-8 md:gap-0">
-            <H2>The dog</H2>
-            <p>
-              Domestic dogs have been companions to people for more than 15,000 years. There are
-              more than 400 different breeds of dogs. PAWS or your local animal shelter has a wide
-              variety of breeds available for adoption. The most popular types are the non-pedigree.
-              They are extra special because they are one-of-a-kind!
-            </p>
-            <p>
-              In general, small dogs live longer than large dogs. Small dogs such as terriers may
-              live 20 years. Giant dogs like Great Danes may live eight years. The average lifespan
-              for a mixed breed dog is 13 to 14 years. By making sure dogs have good food, regular
-              veterinary care, shelter and a loving family, they will live longer, healthier lives.
-            </p>
-          </div>
-        </article>
-      </section>
+            <div className="flex flex-col justify-between gap-8 md:gap-0">
+              <H2>The dog</H2>
+              <p>
+                Domestic dogs have been companions to people for more than 15,000 years. There are
+                more than 400 different breeds of dogs. PAWS or your local animal shelter has a wide
+                variety of breeds available for adoption. The most popular types are the
+                non-pedigree. They are extra special because they are one-of-a-kind!
+              </p>
+              <p>
+                In general, small dogs live longer than large dogs. Small dogs such as terriers may
+                live 20 years. Giant dogs like Great Danes may live eight years. The average
+                lifespan for a mixed breed dog is 13 to 14 years. By making sure dogs have good
+                food, regular veterinary care, shelter and a loving family, they will live longer,
+                healthier lives.
+              </p>
+            </div>
+          </article>
+        </section>
+      )}
     </>
   );
 }
--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,62 +1,69 @@
 import { useEffect, useState } from "react";
-import image1 from "/src/images/gallery/image1.png";
-import image2 from "/src/images/gallery/image2.png";
-import image3 from "/src/images/gallery/image3.png";
-import image4 from "/src/images/gallery/image4.png";
-import image5 from "/src/images/gallery/image5.png";
-import image6 from "/src/images/gallery/image6.png";
-import image7 from "/src/images/gallery/image7.png";
-import image8 from "/src/images/gallery/image8.png";
 import H1 from "../H1";
 
 export default function Gallery() {
-  // Works in dev but not in prod
-  // const imagesImport = import.meta.glob("/src/images/gallery/*");
-  // const images = Object.values(imagesImport).map((image) => image.name);
-
-  const images = [image1, image2, image3, image4, image5, image6, image7, image8];
-  const [imageIndex, setImageIndex] = useState(Math.floor(images.length / 2));
+  const [images, setImages] = useState<string[]>(Array(8).fill(""));
+  const [loading, setLoading] = useState(true);
+  const [middleImageIndex, setMiddleImageIndex] = useState(0);
   const [selectedId, setSelectedId] = useState<number | null>(null);
 
-  // Disable scrolling when image is selected
   useEffect(() => {
-    if (selectedId !== null) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "unset";
-    }
-  }, [selectedId]);
+    const fetchImages = async () => {
+      try {
+        const loadedImages = await Promise.all(
+          Array.from({ length: 8 }, async (_, i) => {
+            const res = await fetch(
+              `https://res.cloudinary.com/dqbe0apqn/image/upload/image${i + 1}`,
+            );
+            if (!res.ok) throw new Error(`Failed to load images`);
+            return `https://res.cloudinary.com/dqbe0apqn/image/upload/image${i + 1}`;
+          }),
+        );
+        setImages(loadedImages);
+      } catch (error) {
+        console.error("Error loading images:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, []);
 
   const prevImage = () => {
-    setImageIndex((prevIndex) => {
+    setMiddleImageIndex((prevIndex) => {
       if (prevIndex === 0) return images.length - 1;
       return prevIndex - 1;
     });
   };
 
   const nextImage = () => {
-    setImageIndex((prevIndex) => {
+    setMiddleImageIndex((prevIndex) => {
       if (prevIndex === images.length - 1) return 0;
       return prevIndex + 1;
     });
   };
 
   return (
-    <section className="flex flex-col gap-4 p-4 transition-opacity duration-300">
+    <section className={`flex flex-col gap-4 p-4 transition-opacity duration-300`}>
       <H1>Gallery</H1>
 
       <article className="mx-auto w-full max-w-screen-xl">
-        <div className="flex gap-16">
+        <div className="flex w-full gap-16">
           {images.map((image, index) => (
-            <img
-              key={image}
-              src={image}
-              alt={`Scrollable image ${index + 1}`}
-              className={`relative left-1/2 rounded-xl transition-all duration-300 ease-in-out sm:min-w-[500px]`}
+            <div
+              key={`scroll-${index}`}
+              className={`relative left-1/2 aspect-square min-w-full rounded-xl transition-transform duration-300 ease-in-out sm:min-w-[500px] ${loading && "animate-pulse bg-gray-400"}`}
               style={{
-                transform: `translateX(calc(-50% + ${-100 * imageIndex}% - ${4 * imageIndex}rem))`,
+                transform: `translateX(calc(-50% + ${-100 * middleImageIndex}% - ${4 * middleImageIndex}rem))`,
               }}
-            />
+            >
+              <img
+                src={image}
+                alt={`Scrollable image ${index + 1}`}
+                className={`w-full rounded-xl transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"}`}
+              />
+            </div>
           ))}
         </div>
 
@@ -72,15 +79,15 @@ export default function Gallery() {
 
       <article className="relative mx-auto grid w-full max-w-screen-xl grid-cols-2 gap-4 md:grid-cols-4">
         {images.map((image, index) => (
-          <div key={image}>
+          <div key={`gallery-${index}`} className="">
             <div
-              className={`flex items-center justify-center ${selectedId === index ? "fixed inset-0 bg-neutral-900/75 p-4" : ""}`}
+              className={`flex w-full items-center justify-center ${selectedId === index ? "fixed inset-0 bg-neutral-900/75 p-4" : "aspect-square rounded-xl"} ${loading && "animate-pulse bg-gray-400"}`}
               onClick={() => setSelectedId(null)}
             >
               <img
                 src={image}
                 alt=""
-                className="w-full max-w-[500px] rounded-xl hover:cursor-pointer hover:brightness-50"
+                className={`w-full max-w-[500px] rounded-xl transition-opacity duration-500 ${selectedId !== index && "hover:cursor-pointer hover:brightness-50"} ${loading ? "opacity-0" : "opacity-100"}`}
                 onClick={(e) => {
                   e.stopPropagation();
                   setSelectedId(index);
