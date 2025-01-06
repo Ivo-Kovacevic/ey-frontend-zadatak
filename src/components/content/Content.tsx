c09301afe3332b3164749b339882fbc9abd37d54
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2025-01-06 17:46:14 +0100

    simplify content skeleton

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import H1 from "../H1";
 import H2 from "../H2";
-import ContentSkeleton from "./ContentSkeleton";
 
 export default function Content() {
   const [loading, setLoading] = useState(true);
@@ -19,59 +18,66 @@ export default function Content() {
   }, []);
 
   return (
-    <>
-      {loading ? (
-        <ContentSkeleton />
-      ) : (
-        <section
-          className={`flex flex-col gap-4 bg-gray-100 p-4 transition-opacity duration-300 ${loading ? "opacity-0" : "opacity-100"}`}
+    <section className="flex flex-col gap-4 bg-gray-100 p-4">
+      <H1>Daily pet facts - Love!</H1>
+
+      <article className="mx-auto flex w-full max-w-screen-xl flex-col gap-6 md:flex-row">
+        <div
+          className={`aspect-square w-full md:max-w-[400px] lg:max-w-[500px] flex-shrink-0 rounded-xl ${loading && "animate-pulse bg-gray-400"}`}
         >
-          <H1>Daily pet facts - Love!</H1>
+          <img
+            src="/src/images/meow.png"
+            alt="closeup picture of a cat"
+            className="w-full rounded-xl"
+          />
+        </div>
 
-          <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row">
-            <img src="/src/images/meow.png" alt="closeup picture of a cat" className="rounded-xl" />
+        <div className="flex flex-col justify-between gap-8 md:gap-0">
+          <H2>The cat</H2>
+          <p>
+            Cats have been domesticated for around 4,000 years. While they were once valued for
+            their hunting abilities, they are now valued for their companionship and loving
+            behaviour.
+          </p>
+          <p>
+            While not well known, the collective nouns used for cats and kittens are a clowder of
+            cats and a kindle of kittens.
+          </p>
+          <p>
+            Our domestic cats are known as little cats. They differ from large cats such as lions
+            and tigers because they are naturally active at night and can purr.
+          </p>
+          <p>Cats are now the most popular pet in the UK and in the US.</p>
+        </div>
+      </article>
 
-            <div className="flex flex-col justify-between gap-8 md:gap-0">
-              <H2>The cat</H2>
-              <p>
-                Cats have been domesticated for around 4,000 years. While they were once valued for
-                their hunting abilities, they are now valued for their companionship and loving
-                behaviour.
-              </p>
-              <p>
-                While not well known, the collective nouns used for cats and kittens are a clowder
-                of cats and a kindle of kittens.
-              </p>
-              <p>
-                Our domestic cats are known as little cats. They differ from large cats such as
-                lions and tigers because they are naturally active at night and can purr.
-              </p>
-              <p>Cats are now the most popular pet in the UK and in the US.</p>
-            </div>
-          </article>
+      <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row-reverse">
+        <div
+          className={`aspect-square w-full md:max-w-[400px] lg:max-w-[500px] flex-shrink-0 rounded-xl ${loading && "animate-pulse bg-gray-400"}`}
+        >
+          <img
+            src="/src/images/woof.png"
+            alt="closeup picture of a dog"
+            className="w-full rounded-xl"
+          />
+        </div>
 
-          <article className="mx-auto flex max-w-screen-xl flex-col gap-6 md:flex-row-reverse">
-            <img src="/src/images/woof.png" alt="closeup picture of a dog" className="rounded-xl" />
-
-            <div className="flex flex-col justify-between gap-8 md:gap-0">
-              <H2>The dog</H2>
-              <p>
-                Domestic dogs have been companions to people for more than 15,000 years. There are
-                more than 400 different breeds of dogs. PAWS or your local animal shelter has a wide
-                variety of breeds available for adoption. The most popular types are the
-                non-pedigree. They are extra special because they are one-of-a-kind!
-              </p>
-              <p>
-                In general, small dogs live longer than large dogs. Small dogs such as terriers may
-                live 20 years. Giant dogs like Great Danes may live eight years. The average
-                lifespan for a mixed breed dog is 13 to 14 years. By making sure dogs have good
-                food, regular veterinary care, shelter and a loving family, they will live longer,
-                healthier lives.
-              </p>
-            </div>
-          </article>
-        </section>
-      )}
-    </>
+        <div className="flex flex-col justify-between gap-8 md:gap-0">
+          <H2>The dog</H2>
+          <p>
+            Domestic dogs have been companions to people for more than 15,000 years. There are more
+            than 400 different breeds of dogs. PAWS or your local animal shelter has a wide variety
+            of breeds available for adoption. The most popular types are the non-pedigree. They are
+            extra special because they are one-of-a-kind!
+          </p>
+          <p>
+            In general, small dogs live longer than large dogs. Small dogs such as terriers may live
+            20 years. Giant dogs like Great Danes may live eight years. The average lifespan for a
+            mixed breed dog is 13 to 14 years. By making sure dogs have good food, regular
+            veterinary care, shelter and a loving family, they will live longer, healthier lives.
+          </p>
+        </div>
+      </article>
+    </section>
   );
 }
